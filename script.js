14f7aaeb54d8aad140a82b486939378ce7fd5f2a
Dana Popa <popa.dana100@gmail.com>
2024-03-14 21:48:33 +0200

    pokedex styled

--- script.js
+++ script.js
@@ -1,18 +1,47 @@
+'use strict';
 let pokemons = [];
+let favorites = [];
+
+const toggleFavorite = (pokemonId) => {
+  const index = favorites.indexOf(pokemonId);
+  if (index === -1) {
+    favorites.push(pokemonId);
+  } else {
+    favorites.splice(index, 1);
+  }
+  localStorage.setItem('favorites', JSON.stringify(favorites));
+  displayFavorites();
+  displayData(pokemons);
+};
 
 const fetchData = () => {
   fetch('https://pokeapi.co/api/v2/pokemon?limit=500&offset=0')
     .then((response) => response.json())
     .then((json) => {
-      pokemons = json.results;
-      displayData(pokemons);
+      const promises = json.results.map((pokemon) => {
+        return fetch(pokemon.url)
+          .then((response) => response.json())
+          .then((data) => {
+            return {
+              id: data.id,
+              name: data.name,
+              height: data.height,
+              weight: data.weight,
+              type: data.types.map((type) => type.type.name).join(', '),
+              image: data.sprites.front_default,
+            };
+          });
+      });
+
+      Promise.all(promises).then((detailedPokemonData) => {
+        pokemons = detailedPokemonData;
+        displayData(pokemons);
+      });
     })
     .catch((error) => {
       console.error('Error fetching data:', error);
     });
 };
-
-fetchData();
 
 const displayData = (data) => {
   const container = document.querySelector('.data');
@@ -22,25 +51,34 @@ const displayData = (data) => {
     const pokemonCard = document.createElement('div');
     pokemonCard.classList.add('pokemon-card');
 
-    const pokemonName = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
-
     const pokemonImage = document.createElement('img');
-    pokemonImage.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${getPokemonId(pokemon)}.png`;
-    pokemonImage.alt = pokemonName;
+    pokemonImage.src = pokemon.image;
+    pokemonImage.alt = pokemon.name;
 
     const pokemonHeading = document.createElement('h2');
-    pokemonHeading.textContent = pokemonName;
+    pokemonHeading.textContent = `${pokemon.id}. ${pokemon.name}`;
+
+    const pokemonDetails = document.createElement('p');
+    pokemonDetails.innerHTML = `
+      <strong>Height:</strong> ${pokemon.height} | 
+      <strong>Weight:</strong> ${pokemon.weight} | 
+      <strong>Type:</strong> ${pokemon.type}
+    `;
+
+    const favoriteButton = document.createElement('button');
+    favoriteButton.textContent = favorites.includes(pokemon.id) ? 'Unfavorite' : 'Favorite';
+    favoriteButton.addEventListener('click', () => {
+      toggleFavorite(pokemon.id);
+      favoriteButton.textContent = favorites.includes(pokemon.id) ? 'Unfavorite' : 'Favorite';
+    });
 
     pokemonCard.appendChild(pokemonImage);
     pokemonCard.appendChild(pokemonHeading);
+    pokemonCard.appendChild(pokemonDetails);
+    pokemonCard.appendChild(favoriteButton);
 
     container.appendChild(pokemonCard);
   });
-};
-
-const getPokemonId = (pokemon) => {
-  const urlParts = pokemon.url.split('/');
-  return urlParts[urlParts.length - 2];
 };
 
 const searchPokemons = (inputValue) => {
@@ -53,3 +91,22 @@ const searchPokemons = (inputValue) => {
 document.querySelector('#search').addEventListener('input', (e) => {
   searchPokemons(e.target.value);
 });
+
+const displayFavorites = () => {
+  const favoritesContainer = document.querySelector('.favorites-list');
+  favoritesContainer.innerHTML = '';
+
+  favorites.forEach((favoriteId) => {
+    const favoritePokemon = pokemons.find((pokemon) => pokemon.id === favoriteId);
+    if (favoritePokemon) {
+      const favoriteItem = document.createElement('div');
+      favoriteItem.textContent = `${favoritePokemon.id}. ${favoritePokemon.name}`;
+      favoritesContainer.appendChild(favoriteItem);
+    }
+  });
+};
+
+favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+displayFavorites();
+fetchData();
+
